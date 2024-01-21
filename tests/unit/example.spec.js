d7b1fdef667c54aaee71bb154dad80fd752f45d3
Sardor Ergashev <127672615+Ergashev-Sardor@users.noreply.github.com>
2024-01-21 17:22:12 +0500

    refactor: remove unnecessary code and image

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from "@vue/test-utils";
+/*import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
@@ -9,4 +9,4 @@ describe("HelloWorld.vue", () => {
     });
     expect(wrapper.text()).toMatch(msg);
   });
-});
+});*/
