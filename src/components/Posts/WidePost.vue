55192fd4d3f459eeafab496c56108baf8e22e51d
Sardor <sardorergashev977@gmail.com>
2024-07-02 22:05:34 +0500

    feat: added an image and a block of text

--- src/components/Posts/WidePost.vue
+++ src/components/Posts/WidePost.vue
@@ -7,13 +7,18 @@
           <img src="https://archakov.im/uploads/1666542894623.jpg" alt="post" />
         </div>
       </div>
-      <div>
-        <div>
-          <i style="background-color: rgb(132, 114, 214)"></i>
-          text
+      <div class="post-info">
+        <div class="post-info__tag">
+          <i></i>
+          работа
         </div>
-        <h2></h2>
-        <p></p>
+        <h2 class="post-info__title">
+          История о том, как наш новый тимлид нифига не зная стал тимлидом
+        </h2>
+        <p class="post-info__description">
+          В этой статье я расскажу про боль, с которой я столкнулся за последние
+          несколько дней. История о том, как в на...
+        </p>
       </div>
     </article>
   </router-link>
@@ -22,14 +27,28 @@
 <script></script>
 
 <style lang="css" scoped>
+@keyframes animate-in {
+  0% {
+    opacity: 0;
+    visibility: hidden;
+    transform: translateY(15px);
+  }
+  100% {
+    opacity: 1;
+    visibility: visible;
+    transform: translateY(0);
+  }
+}
+
 .animate {
   display: inline-block;
   opacity: 0;
   visibility: hidden;
   transform: translateY(15px);
-  animation: animateIn 0.6s cubic-bezier(0.25, 0.75, 0.5, 1.25) forwards;
+  animation: animate-in 0.6s cubic-bezier(0.25, 0.75, 0.5, 1.25) forwards;
   animation-iteration-count: 1;
 }
+
 .post {
   display: flex;
   height: 350px;
@@ -48,6 +67,10 @@
   box-shadow: 0 50px 50px -20px rgba(50, 50, 93, 0.03),
     0 30px 10px -30px rgba(0, 0, 0, 0.06);
   border: 1px solid #eae8e8;
+}
+
+.post:hover .post-image .post-image__wrapper img:last-of-type {
+  opacity: 0.35;
 }
 
 @keyframes post-image-loader {
@@ -74,34 +97,74 @@
 
 .post-image__wrapper {
   width: 480px;
-  height: 100px;
+  height: 100%;
   opacity: 1;
   transition: opacity 0.5s ease-in-out;
 }
 
-.post-image__wrapper:first-of-type {
-  width: 100px;
-  height: 100px;
+.post-image__wrapper img:first-of-type {
+  width: 100%;
+  height: 100%;
   object-fit: cover;
   position: relative;
   z-index: 1;
   border-radius: 15px 0 0 15px;
 }
 
-.post-image__wrapper:last-of-type {
-  opacity: 0.35;
+.post-image__wrapper img:last-of-type {
   transition: all 0.15s ease-in-out;
   width: 95%;
-  height: 100px;
+  height: 100%;
   object-fit: cover;
   position: absolute;
   left: 49%;
   top: 10px;
   transform: translateX(-50%);
   filter: blur(25px) saturate(12);
+  opacity: 0;
 }
 
 .post-image__wrapper.visible {
   opacity: 1;
+}
+
+.post-info {
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 40px;
+}
+
+.post-info__tag {
+  display: inline-block;
+  text-transform: capitalize;
+}
+
+i {
+  display: inline-block;
+  margin-right: 10px;
+  height: 10px;
+  width: 10px;
+  border-radius: 30px;
+  font-style: italic;
+  background-color: rgb(132, 114, 214);
+}
+
+.post-info__title {
+  font-weight: 800;
+  margin-top: 15px;
+  margin-bottom: 10px;
+  color: #000;
+  font-size: 32px;
+  line-height: 40px;
+}
+
+.post-info__description {
+  margin: 0;
+  font-family: Inter, system-ui, Helvetica Neue, Helvetica, sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  color: #000;
 }
 </style>
