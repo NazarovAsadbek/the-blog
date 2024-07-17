cd2d50a55f19b4f08e5e415737f7047c479890d0
Sardor <sardorergashev977@gmail.com>
2024-07-17 21:48:08 +0500

    feat: we added narrow posts and notes.

---
+++ src/views/Post/PostView.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="post-wrapper">
+    12345
+    <img src="https://archakov.im/uploads/1666542894623.jpg" alt="post" />
+  </section>
+</template>
+
+<script></script>
+
+<style lang="css" scoped>
+.post-wrapper {
+  box-sizing: border-box;
+  padding: 70px 90px;
+  border-radius: 20px;
+  box-shadow: 0 60px 30px hsla(0, 0%, 40%, 0.05);
+  background-color: #fff;
+}
+</style>
